bcc43246fdf9c335fc95cf6da8b8b79060e0909c
Eluro Alex <alexeluro@gmail.com>
2021-03-03 00:39:23 +0100

    customized the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 
@@ -33,8 +34,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=Lagos&units=metric&appid=38f556ee904eef3704f50e0f34d43cfe";
+  final String BASE_URL =
+      "http://api.openweathermap.org/data/2.5/weather?q=Lagos&units=metric&appid=38f556ee904eef3704f50e0f34d43cfe";
 
   var temp;
   var description;
@@ -42,16 +43,16 @@ class _MyHomePageState extends State<MyHomePage> {
   var humidity;
   var windSpeed;
 
-  Future getWeather() async{
+  Future getWeather() async {
     http.Response response = await http.get(BASE_URL);
     var results = jsonDecode(response.body);
 
     setState(() {
-        this.temp = results["main"]["temp"];
-        this.description = results["weather"][0]["description"];
-        this.currently = results["weather"][0]["main"];
-        this.humidity = results["main"]["humidity"];
-        this.windSpeed = results["wind"]["speed"];
+      this.temp = results["main"]["temp"];
+      this.description = results["weather"][0]["description"];
+      this.currently = results["weather"][0]["main"];
+      this.humidity = results["main"]["humidity"];
+      this.windSpeed = results["wind"]["speed"];
     });
   }
 
@@ -67,9 +68,15 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Column(
       children: <Widget>[
         Container(
-          height: MediaQuery.of(context).size.height / 3,
+          height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          color: Colors.red,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              colorFilter: ColorFilter.mode(Colors.deepPurpleAccent, BlendMode.color),
+              image: AssetImage("assets/images/wetglass2.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -77,60 +84,82 @@ class _MyHomePageState extends State<MyHomePage> {
               Padding(
                   padding: EdgeInsets.only(bottom: 10.0),
                   child: Text(
-                    "Weather in Nigeria",
+                    "Nigeria",
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 14.0,
                         fontWeight: FontWeight.w600),
                   )),
               Text(
-                temp != null ? temp.toString()+"\u00B0 c": "Loading",
+                temp != null ? temp.toString() + "\u00B0 c" : "Loading",
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 40.0,
+                    fontSize: 70.0,
                     fontWeight: FontWeight.w600),
               ),
-              Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    description != null ? description.toString() : "Loading...",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w600),
-                  )),
+              SizedBox(
+                height: 60.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        FaIcon(FontAwesomeIcons.cloud, color: Colors.white),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          description != null
+                              ? description.toString()
+                              : "Loading...",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        FaIcon(FontAwesomeIcons.sun, color: Colors.white),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          humidity != null ? humidity.toString() : "Loading...",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        FaIcon(
+                          FontAwesomeIcons.wind,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          windSpeed != null
+                              ? windSpeed.toString()
+                              : "Loading...",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
-        Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.thermometer),
-                    title: Text("Temperature"),
-                    trailing: Text(temp != null ? temp.toString()+" \u00B0 c" : "Loading..."),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.cloud),
-                    title: Text("Weather"),
-                    trailing: Text(description != null ? description.toString() : "Loading..."),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.sun),
-                    title: Text("Humidity"),
-                    trailing: Text(humidity != null ? humidity.toString() : "Loading..."),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.wind),
-                    title: Text("Wind Speed"),
-                    trailing: Text(windSpeed != null ? windSpeed.toString() : "Loading..."),
-                  ),
-                ],
-              ),
-            )
-        )
       ],
     ));
   }
